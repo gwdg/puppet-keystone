c023995ee1fa4abf8aba598c603efd4c1b88a97d
Maik Srba <msrba@gwdg.de>
2018-07-10 10:25:51 +0200

    fix apache2 oidc config

--- manifests/federation/openidc_httpd_configuration.pp
+++ manifests/federation/openidc_httpd_configuration.pp
@@ -16,7 +16,7 @@ define keystone::federation::openidc_httpd_configuration (
   $port              = undef,
   $keystone_endpoint = undef
 ) {
-  $openidc_redirect_uri = "${keystone_endpoint}/v3/auth/OS-FEDERATION/websso/openidc/redirect"
+  $openidc_redirect_uri = "${keystone_endpoint}/v3/auth/OS-FEDERATION/websso/oidc/redirect"
   concat::fragment { "configure_openidc_on_port_${port}":
     target  => "${keystone::wsgi::apache::priority}-keystone_wsgi_${title}.conf",
     content => template('keystone/openidc.conf.erb'),
