7b04bdd6297e855c72466bb5337b1da1b4d1d516
Piotr Kasprzak <piotr.kasprzak@gwdg.de>
2020-02-07 15:49:02 +0100

    Fix policy.json owner / group

--- spec/classes/keystone_policy_spec.rb
+++ spec/classes/keystone_policy_spec.rb
@@ -19,6 +19,8 @@ describe 'keystone::policy' do
       is_expected.to contain_openstacklib__policy__base('context_is_admin').with({
         :key   => 'context_is_admin',
         :value => 'foo:bar'
+        :file_user  => 'keystone',
+        :file_group => 'keystone',
       })
       is_expected.to contain_oslo__policy('keystone_config').with(
         :policy_file => '/etc/keystone/policy.json',
